cb9ae9389a06906eb9d8029921114ece24f7e160
Anthony Isensee <60198052+anthonyisensee@users.noreply.github.com>
2024-08-08 12:26:55 -0400

    feat: adds validation for number of characters and info on repeated possible characters

--- js/Main.mjs
+++ js/Main.mjs
@@ -1,7 +1,9 @@
+import { addInputValidation } from './InputValidation.mjs'
 import { addPasswordGeneratorFunctionality } from './PasswordGenerator.mjs'
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    addInputValidation()
     addPasswordGeneratorFunctionality()
 
 })
--- js/PasswordGenerator.mjs
+++ js/PasswordGenerator.mjs
@@ -1,3 +1,5 @@
+import { getMapOfCharactersFromString } from "./CharacterMapper.mjs"
+
 export function addPasswordGeneratorFunctionality() {
 
     const passwordInput = document.getElementById("password")
@@ -9,29 +11,7 @@ export function addPasswordGeneratorFunctionality() {
     generateNewPasswordButton.addEventListener("click", e => {
 
         const possibleCharactersString = possibleCharactersTextarea.value
-        const characterMap = new Map()
-
-        // Loop over all individual characters
-        for (let i = 0; i < possibleCharactersString.length; i++) {
-
-            // Simplify the reference to the individual character
-            const character = possibleCharactersString[i]
-
-            const characterInMap = characterMap.get(character) !== undefined
-            if (!characterInMap) {
-                
-                // Add the character to the map
-                characterMap.set(character, 1)
-                
-            } else {
-                
-                // Increment the number of references if it's already there
-                const numberOfReferences = characterMap.get(character)
-                characterMap.set(character, numberOfReferences + 1)
-
-            }
-
-        }
+        const characterMap = getMapOfCharactersFromString(possibleCharactersString)
 
         const numberArray = new Uint16Array(numberOfCharactersInput.value)
         const randomNumbers = window.crypto.getRandomValues(numberArray)
